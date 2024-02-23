934bc40f0788de91b520018a826035d018298b8b
Abd-El-Rahman-Mohamed <90394194+Abd-El-Rahman-Mohamed@users.noreply.github.com>
2024-02-23 15:22:55 +0200

    Create addTwo.js
    A simple code snippet to export a function in a default export way.

---
+++ Course Introduction/JavaScript modules imports and exports/default exports/addTwo.js
@@ -0,0 +1,4 @@
+// In the example, we exported the function in a default export way by adding the `export default` keywords in front of the `addTwo` function in declaration.
+export default function addTwo(a, b) {
+  console.log(a + b);
+}
